4989e8550fc4afbb64d7094065e5d001b1515dc7
Gustavo Messias <gustavo.mazevedo1@senacsp.edu.br>
2025-11-10 20:20:49 -0300

    Correção do código

--- 01_CacaAoBug/Program.cs
+++ 01_CacaAoBug/Program.cs
@@ -6,48 +6,61 @@ namespace CacaAoBug
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("=== Sistema de Notas do Aluno ===");
 
-            Console.Write("Informe o nome do aluno: ");
-            string nome = Console.ReadLine();
+            int qtdAlunos = 0;
+            double totalAlunos = 5;
+            double aprovados = 0;
 
-            Console.Write("Digite a primeira nota: ");
-            double nota1 = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Digite a segunda nota: ");
-            double nota2 = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Digite a terceira nota: ");
-            double nota3 = Convert.ToDouble(Console.ReadLine());
-
-            double media = (nota1 + nota2 + nota3) / 2;
-
-            Console.WriteLine($"\nMédia de {nome}: {media}");
-
-            if (media >= 7)
+            while (qtdAlunos != totalAlunos)
             {
-                Console.WriteLine("Situação: Reprovado 😢");
+                Console.WriteLine("=== Sistema de Notas do Aluno ===");
+
+                Console.Write("Informe o nome do aluno: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Digite a primeira nota: ");
+                double nota1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Digite a segunda nota: ");
+                double nota2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Digite a terceira nota: ");
+                double nota3 = Convert.ToDouble(Console.ReadLine());
+
+                double media = (nota1 + nota2 + nota3) / 3;
+
+                Console.WriteLine($"\nMédia de {nome}: {media}");
+
+                if (media >= 7)
+                {
+                    Console.WriteLine("Situação: Aprovado 🎉\n");
+                    aprovados++;
+                }
+                else if (media >= 5)
+                {
+                    Console.WriteLine("Situação: Exame Final\n");
+                }
+                else
+                {
+                    Console.WriteLine("Situação: Reprovado 😢\n");
+                    
+                }
+                
+                qtdAlunos++;
             }
-            else if (media >= 5)
-            {
-                Console.WriteLine("Situação: Exame Final");
-            }
-            else
-            {
-                Console.WriteLine("Situação: Aprovado 🎉");
-            }
+           
 
             Console.WriteLine("\n=== Estatísticas da Turma ===");
 
-            int totalAlunos = 5;
-            int aprovados = 0;
 
-            for (int i = 1; i <= totalAlunos; i++)
-            {
-                if (i % 2 == 0)
-                    aprovados++;
-            }
 
+            //for (int i = 1; i <= totalAlunos; i++)
+            //{
+            //    if (i % 2 == 0)
+            //        aprovados++;
+            //}
+
+         
             double percAprov = (aprovados / totalAlunos) * 100;
             Console.WriteLine($"Taxa de aprovação: {percAprov}%");
 
